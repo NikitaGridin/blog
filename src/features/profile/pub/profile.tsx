34f31521e2c0fd0d5d91fe96bf98b67e361cc049
Nikita <aruisialalin@yandex.ru>
2024-06-09 22:33:05 +0300

    fix: Profile, check session

--- src/features/profile/pub/profile.tsx
+++ src/features/profile/pub/profile.tsx
@@ -4,19 +4,24 @@ import { LogoutBtn } from '../_ui/logout-btn'
 import { ProfileNav } from '../_ui/profile-nav'
 
 export async function Profile({ userId }: { userId: string }) {
-  const session = await getSession()
-  const user = await getUser({ id: userId })
-  if (!user) return <div>Пользователь не найден</div>
+  try {
+    const session = await getSession().catch(() => null) // Если getSession выбрасывает ошибку, вернуть null
+    const user = await getUser({ id: userId })
 
-  const isOwner = session ? user.id === session.id : false
+    if (!user) return <div>Пользователь не найден</div>
 
-  return (
-    <div className="bg-white rounded-md p-4 dark:bg-gray-900 space-y-4">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-bold">{isOwner ? 'Профиль' : user.name}</h1>
-        {isOwner && <LogoutBtn />}
+    const isOwner = session ? user.id === session.id : false
+
+    return (
+      <div className="bg-white rounded-md p-4 dark:bg-gray-900 space-y-4">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-bold">{isOwner ? 'Профиль' : user.name}</h1>
+          {isOwner && <LogoutBtn />}
+        </div>
+        <ProfileNav userId={user.id} isOwner={isOwner} />
       </div>
-      <ProfileNav userId={user.id} isOwner={isOwner} />
-    </div>
-  )
+    )
+  } catch (error) {
+    return <div>Ошибка загрузки профиля</div>
+  }
 }
